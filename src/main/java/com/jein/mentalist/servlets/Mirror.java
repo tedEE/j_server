8f668013aa8cdaf16f23f47cbda2a0725897a3a5
Стас <cjb2018@yandex.ru>
2020-02-01 20:43:39 +0700

    обработка любого количествк гет параметров

--- src/main/java/com/jein/mentalist/servlets/Mirror.java
+++ src/main/java/com/jein/mentalist/servlets/Mirror.java
@@ -7,17 +7,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Mirror extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       System.out.println(req.getParameterMap().toString());
 
         Map<String, Object> getVar = new HashMap<String, Object>();
+        int flag = 1;
 
-        getVar.put("getParametrs" , req.getParameterMap().get("key")[0]);
+        for (String[] v : req.getParameterMap().values()) {
+            getVar.put("getParametrs" + flag , v[v.length -1]);
+            flag ++;
+        }
+
+
 
         resp.getWriter().println(PageGenerator.instance().getPage("page.html", getVar));
     }
